a403956d74ec982ee3133de4e839a2dd37812702
ashWhiteHat <phantomofrotten@gmail.com>
2023-01-22 13:18:21 +0900

    init commit

---
+++ target/debug/deps/zero_network-869a9dc8b51853f6.d
@@ -0,0 +1,7 @@
+/Users/shinsakuashizawa/node/target/debug/deps/zero_network-869a9dc8b51853f6.rmeta: src/lib.rs
+
+/Users/shinsakuashizawa/node/target/debug/deps/libzero_network-869a9dc8b51853f6.rlib: src/lib.rs
+
+/Users/shinsakuashizawa/node/target/debug/deps/zero_network-869a9dc8b51853f6.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/deps/zero_network-eb94bf3e6be588b6.d
@@ -0,0 +1,5 @@
+/Users/shinsakuashizawa/node/target/debug/deps/zero_network-eb94bf3e6be588b6: src/lib.rs
+
+/Users/shinsakuashizawa/node/target/debug/deps/zero_network-eb94bf3e6be588b6.d: src/lib.rs
+
+src/lib.rs:
