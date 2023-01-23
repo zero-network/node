7b1ec428961686add2f2d1138391ed9ab610dfc1
ashWhiteHat <phantomofrotten@gmail.com>
2023-01-23 17:10:55 +0900

    setup node

--- chain/scripts/init.sh
+++ chain/scripts/init.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
-# This script meant to be run on Unix/Linux based systems
-set -e
 
-echo "*** Initializing WASM build environment"
-
-if [ -z $CI_PROJECT_NAME ] ; then
-   rustup update nightly
-   rustup update stable
-fi
-
-rustup target add wasm32-unknown-unknown --toolchain nightly
+rustup default nightly-2023-01-22
+rustup target add wasm32-unknown-unknown --toolchain nightly-2023-01-22
+cargo build
--- chain/scripts/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-rustup default nightly-2022-11-14
-rustup target add wasm32-unknown-unknown --toolchain nightly-2022-11-14
-cargo build
