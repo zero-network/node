19f3059f099e3da2f7cedec1defe3760f91e09e1
ashWhiteHat <phantomofrotten@gmail.com>
2023-01-23 16:43:01 +0900

    update version

--- chain/scripts/setup.sh
+++ chain/scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-rustup default nightly-2022-07-27
-rustup target add wasm32-unknown-unknown --toolchain nightly-2022-07-27
+rustup default nightly-2022-11-14
+rustup target add wasm32-unknown-unknown --toolchain nightly-2022-11-14
 cargo build
--- target/debug/deps/zero_network-eb94bf3e6be588b6.d
+++
@@ -1,5 +0,0 @@
-/Users/shinsakuashizawa/node/target/debug/deps/zero_network-eb94bf3e6be588b6: src/lib.rs
-
-/Users/shinsakuashizawa/node/target/debug/deps/zero_network-eb94bf3e6be588b6.d: src/lib.rs
-
-src/lib.rs:
